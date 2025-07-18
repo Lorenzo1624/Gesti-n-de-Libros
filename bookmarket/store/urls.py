111ce6a0d21d736076d141aeee2313189ab06e8e
Lorenzo1624 <155730779+Lorenzo1624@users.noreply.github.com>
2025-07-18 16:06:19 -0400

    Delete bookmarket directory

--- bookmarket/store/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.book_list, name='book_list'),
-    path('books/<int:pk>/', views.book_detail, name='book_detail'),
-    path('books/new/', views.book_create, name='book_create'),
-    path('books/<int:pk>/buy/', views.order_book, name='order_book'),
-]
